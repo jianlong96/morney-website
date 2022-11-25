67eec418acd8b2309651e9a62e34302df7819cb6
jianlong96 <liujianlong_hw_hrb@163.com>
2022-11-25 23:08:57 +0800

    dist

---
+++ precache-manifest.194b5c74e529032058f98956188af003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9857894e4d1dc4c452ba",
+    "url": "/morney-website/css/app.ade57f06.css"
+  },
+  {
+    "revision": "01dd92c4c062c708f0f952d03f0a7605",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "9857894e4d1dc4c452ba",
+    "url": "/morney-website/js/app.22ada6f4.js"
+  },
+  {
+    "revision": "71a6b04010c5b4de42a6",
+    "url": "/morney-website/js/chunk-vendors.1e1910f3.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
